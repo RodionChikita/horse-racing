168893905f57508de9901379a98916e8d733d771
rodioncikita <chikita.rodion@gmail.com>
2024-09-21 22:30:39 +0300

    init

---
+++ src/main/java/ru/mpu/horse_racings/HorseRacingsApplication.java
@@ -0,0 +1,13 @@
+package ru.mpu.horse_racings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseRacingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorseRacingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mpu/horse_racings/HorseRacingsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mpu.horse_racings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorseRacingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
