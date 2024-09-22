9a66624bcfea076ac1599b62a1fdea2a7ecc2628
rodioncikita <chikita.rodion@gmail.com>
2024-09-22 16:34:13 +0300

    fix

--- src/main/java/ru/mpu/horse_races/domain/entities/Owner.java
+++ src/main/java/ru/mpu/horse_races/domain/entities/Owner.java
@@ -1,19 +1,13 @@
 package ru.mpu.horse_races.domain.entities;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
 
 @Getter
 @Setter
