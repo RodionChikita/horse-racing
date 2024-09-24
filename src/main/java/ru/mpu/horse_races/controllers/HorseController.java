4765e2e94763f195a64ce98fe3b2801be2e24caf
rodioncikita <chikita.rodion@gmail.com>
2024-09-25 00:46:24 +0300

    fix

--- src/main/java/ru/mpu/horse_races/controllers/HorseController.java
+++ src/main/java/ru/mpu/horse_races/controllers/HorseController.java
@@ -1,13 +1,21 @@
 package ru.mpu.horse_races.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.mpu.horse_races.domain.dtos.HorseDto;
 import ru.mpu.horse_races.services.HorseService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/horse")
 public class HorseController {
     private final HorseService horseService;
+    @GetMapping
+    public List<HorseDto> getAllHorses() {
+        return horseService.findAll();
+    }
 }
