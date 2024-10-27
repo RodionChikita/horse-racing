2145c86142e30513a9e779e724510d2e30219992
RodionChikita <chikita.rodion@gmail.com>
2024-10-27 18:09:33 +0300

    fix

--- frontend/horse-races/src/app/horse/horse-list/horse-list.component.ts
+++ frontend/horse-races/src/app/horse/horse-list/horse-list.component.ts
@@ -1,79 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HorseService } from '../horse.service';
-import {OwnerDto} from "../../owner/owner.models";
-import {CreateOrUpdateHorseDtoRq, HorseDto, GenderEnum} from "../horse.models";
-import {OwnerService} from "../../owner/owner.service";
+import { OwnerService } from '../../owner/owner.service';
+import { HorseDto, CreateOrUpdateHorseDtoRq, GenderEnum } from '../horse.models';
+import { OwnerDto } from '../../owner/owner.models';
 
 @Component({
-    selector: 'app-horse-list',
-    templateUrl: './horse-list.component.html',
-    styleUrls: ['./horse-list.component.scss']
+  selector: 'app-horse-list',
+  templateUrl: './horse-list.component.html',
+  styleUrls: ['./horse-list.component.scss']
 })
 export class HorseListComponent implements OnInit {
-    horses: HorseDto[] = [];
-    owners: OwnerDto[] = [];
-    currentHorse: CreateOrUpdateHorseDtoRq = { nickname: '', genderEnum: GenderEnum.MALE, age: 0, ownerId: 0 };
+  horseForm: FormGroup;
+  horses: HorseDto[] = [];
+  owners: OwnerDto[] = [];
 
-    constructor(private horseService: HorseService, private ownerService: OwnerService) {}
+  constructor(
+    private fb: FormBuilder,
+    private horseService: HorseService,
+    private ownerService: OwnerService
+  ) {
+    this.horseForm = this.fb.group({
+      nickname: ['', Validators.required],
+      genderEnum: ['MALE', Validators.required],
+      age: [0, Validators.required],
+      ownerId: [null, Validators.required]
+    });
+  }
 
-    ngOnInit() {
-        this.loadHorses();
-        this.loadOwners();
-    }
+  ngOnInit() {
+    this.loadHorses();
+    this.loadOwners();
+  }
 
-    loadOwners() {
-        this.ownerService.findAll().subscribe((data: OwnerDto[]) => {
-            this.owners = data;
-            console.log('Loaded owners:', this.owners); // Лог для проверки данных
-        });
-    }
+  loadHorses() {
+    this.horseService.findAll().subscribe((data) => {
+      this.horses = data;
+    });
+  }
 
-    loadHorses() {
-        this.horseService.findAll().subscribe((data) => {
-            this.horses = data;
-        });
-    }
+  loadOwners() {
+    this.ownerService.findAll().subscribe((data) => {
+      this.owners = data;
+    });
+  }
 
-addHorse(e: any) {
-    const newHorse: CreateOrUpdateHorseDtoRq = { ...e.data, ownerId: e.data.ownerId };
-    if (!this.validateHorse(newHorse)) {
-        console.error('Validation failed');
-        e.cancel = true;
-        return;
-    }
-
-    e.promise = this.horseService.insert(newHorse).toPromise().then(
+  saveHorse() {
+    if (this.horseForm.valid) {
+      const newHorse: CreateOrUpdateHorseDtoRq = this.horseForm.value;
+      this.horseService.insert(newHorse).subscribe(
         () => {
-            this.loadHorses();
+          this.loadHorses();
         },
-        (error: any) => {
-            console.error('Failed to add horse', error);
-            e.cancel = true;
+        (error) => {
+          console.error('Failed to save horse', error);
         }
-    );
-}
-
-updateHorse(e: any) {
-    const updatedHorse: CreateOrUpdateHorseDtoRq = { ...e.oldData, ...e.newData, ownerId: e.newData.ownerId || e.oldData.ownerId };
-    if (!this.validateHorse(updatedHorse)) {
-        console.error('Validation failed');
-        e.cancel = true;
-        return;
+      );
     }
-
-    e.promise = this.horseService.update(updatedHorse).toPromise().then(
-        () => {
-            this.loadHorses();
-        },
-        (error: any) => {
-            console.error('Failed to update horse', error);
-            e.cancel = true;
-        }
-    );
-}
-
-private validateHorse(horse: CreateOrUpdateHorseDtoRq): boolean {
-    return !!horse.nickname && !!horse.genderEnum && horse.age != null && horse.ownerId != null;
+  }
 }
 
     deleteHorse(e: any) {
