e484d15e1978771c1a2b27a10b5606e723d146f7
RodionChikita <chikita.rodion@gmail.com>
2024-11-30 02:51:46 +0300

    feat add keycloack

--- frontend/horse-races/src/app/app.module.ts
+++ frontend/horse-races/src/app/app.module.ts
@@ -15,43 +15,36 @@ import {
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { OwnerListComponent } from './owner/owner-list/owner-list.component';
-import {HttpClientModule} from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { HorseListComponent } from './horse/horse-list/horse-list.component';
 import { RaceComponent } from './race/race.component';
 import {RaceResultComponent} from './race-result/race-result.component';
 import { JockeyListComponent } from './jockey/jockey-list/jockey-list.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    OwnerListComponent,
-    HorseListComponent,
-    RaceComponent,
-    RaceResultComponent,
-    JockeyListComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    DxDataGridModule,
-    DxCheckBoxModule,
-    ReactiveFormsModule,
-    DxTextBoxModule,
-    DxButtonModule,
-    DxPopupModule,
-    DxDateBoxModule,
-    DxSelectBoxModule,
-    AppRoutingModule
-  ],
-  providers: [OwnerListComponent, HorseListComponent,
-      {
-        provide: APP_INITIALIZER,
-        useFactory: initializer,
-        deps: [KeycloakService],
-        multi: true
-      }],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        OwnerListComponent,
+        HorseListComponent,
+        RaceComponent,
+        RaceResultComponent,
+        JockeyListComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        DxDataGridModule,
+        DxCheckBoxModule,
+        ReactiveFormsModule,
+        DxTextBoxModule,
+        DxButtonModule,
+        DxPopupModule,
+        DxDateBoxModule,
+        DxSelectBoxModule,
+        AppRoutingModule], providers: [OwnerListComponent, HorseListComponent,
+        {
+            provide: APP_INITIALIZER,
+            useFactory: initializer,
+            deps: [KeycloakService],
+            multi: true
+        }, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
