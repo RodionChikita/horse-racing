5f5965a9354f4be2a8fc83441df992e363824b6f
RodionChikita <chikita.rodion@gmail.com>
2024-12-06 22:01:11 +0300

    feat add keycloack

--- frontend/horse-races/src/app/app-routing.module.ts
+++ frontend/horse-races/src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {OwnerListComponent} from "./owner/owner-list/owner-list.component";
-import {HorseListComponent} from "./horse/horse-list/horse-list.component";
-import {RaceComponent} from "./race/race.component";
-import {RaceResultComponent} from "./race-result/race-result.component";
-import {JockeyListComponent} from "./jockey/jockey-list/jockey-list.component";
+import { AuthGuard } from './auth/auth.guard';
+import { AccessDeniedComponent } from './access-denied/access-denied.component';
+import { OwnerListComponent } from './owner/owner-list/owner-list.component';
+import { HorseListComponent } from './horse/horse-list/horse-list.component';
+import { RaceComponent } from './race/race.component';
+import { RaceResultComponent } from './race-result/race-result.component';
+import { JockeyListComponent } from './jockey/jockey-list/jockey-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'owner', pathMatch: 'full'},
-  { path: 'horse', component: HorseListComponent },
-  { path: 'owner', component: OwnerListComponent },
-  { path: 'jockey', component: JockeyListComponent },
-  { path: 'race', component: RaceComponent },
-  { path: 'race-results/:id', component: RaceResultComponent }
-
+  { path: '', redirectTo: 'owner', pathMatch: 'full' },
+  { path: 'horse', component: HorseListComponent, canActivate: [AuthGuard], data: { roles: ['user'] } },
+  { path: 'owner', component: OwnerListComponent, canActivate: [AuthGuard], data: { roles: ['user'] } },
+  { path: 'jockey', component: JockeyListComponent, canActivate: [AuthGuard], data: { roles: ['user'] } },
+  { path: 'race', component: RaceComponent, canActivate: [AuthGuard] },
+  { path: 'race-results/:id', component: RaceResultComponent, canActivate: [AuthGuard], data: { roles: ['user'] } },
+  { path: 'access-denied', component: AccessDeniedComponent }
 ];
 
 @NgModule({
